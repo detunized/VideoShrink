de3532b51e9788692b6f0caaf02320197a21b8db
Dmitry Yakimenko <detunized@gmail.com>
2018-11-19 13:39:38 +0100

    Process shared video uri

--- app/src/main/java/net/detunized/videoshrink/MainActivity.kt
+++ app/src/main/java/net/detunized/videoshrink/MainActivity.kt
@@ -1,14 +1,33 @@
 package net.detunized.videoshrink
 
 import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import net.ypresto.androidtranscoder.MediaTranscoder
-import net.ypresto.androidtranscoder.format.MediaFormatStrategyPresets
+import android.os.Parcelable
+import android.util.Log
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        processSendIntent()
+    }
+
+    private fun processSendIntent() {
+        if (intent.action == Intent.ACTION_SEND && intent.type.startsWith("video/")) {
+            (intent.getParcelableExtra<Parcelable>(Intent.EXTRA_STREAM) as? Uri)?.let  {
+                processVideo(it)
+            }
+        }
+    }
+
+    private fun processVideo(uri: Uri) {
+        Log.d(TAG, "Received a video ${uri.path}")
+    }
+
+    companion object {
+        private const val TAG = "video-shrink"
     }
 }
